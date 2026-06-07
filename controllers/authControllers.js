c8d36eac018df4ab5339798fdd851de8a2eb3f8b
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-06-07 20:23:36 +0400

    User signin controller has been updated.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -28,7 +28,7 @@ const signupUserController = async (req, res) => {
 
 
 // USER SIGNIN CONTROLLER FUNCTION
-const signinUserController = (req, res) => {
+const signinUserController = async(req, res) => {
     // GET THE USERNAME & PASSWORD FROM REQUEST BODY THEN VALIDATE THEM.
     const {username, password} = req.body;
     if (!username || !password) {
@@ -36,30 +36,19 @@ const signinUserController = (req, res) => {
     }
     // CHECK THE USER EXISTS OR NOT.
     const user = checkUser(username);
-    if (user) {
-        // IF USER EXISTS THEN COMPARE THE PASSWORD.
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-            // IF ANY ERROR WHILE COMPARING.
-            if (err) {
-                return res.status(500).send("Password doesn't match.");
-            }
-            // IF PASSWORD MATCH SUCCESSFULLY THEN GENERATE A JWT TOKEN AND SEND TO THE USER.
-            if (isMatch) {
-                // SET USERNAME & USER ID TO TOKEN AS PAYLOAD.
-                const payload = {
-                    userId: user.id,
-                    username: user.username
-                };
-                // GENERATE A JWT TOKEN FOR THE USER.
-                const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
-                res.status(200).send({ message: "Signin successful.", token });
-            } else {
-                res.status(401).send("Invalid username or password.");
-            }
-        });
-    } else {
-        res.status(404).send("User not found.");
-    }
+    if (!user) return res.status(401).json({message: "user not found!"});
+
+    // PASSWORD VALIDATION
+    const validPassword =   await bcrypt.compare(password, user.password);
+    if (!validPassword) return res.status(401).json({message: "Password doesn't match.!"});
+
+    const payload = {
+        userId: user.id,
+        username: user.username
+    };
+    // GENERATE A JWT TOKEN FOR THE USER.
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    res.status(200).json({ message: "Signin successful.", token });
 };
 
 
--- services/authServices.js
+++
@@ -1,4 +0,0 @@
-const userServices = () => {
-    // This function will handle user sign up logic
-    const userSignUp = (username, password) => {}
-};
